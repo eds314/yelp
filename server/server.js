8d50a34d53943bd45b619852a74858ffa604ee0e
eds314 <eduardoramirez314@gmail.com>
2021-05-28 16:50:43 -0500

    connected express and postgres

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ require('dotenv').config()
 
 const express = require('express')
 
+const db = require('./db')
+
 const morgan = require('morgan')
 
 const app = express()
@@ -17,25 +19,62 @@ app.use(express.json());
 
 
 //Get all Restuarants rout handler
-app.get("/api/v1/getRestaurants", (req, res) => {
-  console.log("route handler ran")
+app.get("/api/v1/restaurants", async (req, res) => {
+
+ const results = await db.query("select * from restaurants")
+  console.log(results)
   res.status(200).json({
     status: 'success',
     data: {
       restaurant: ["McDonalds", "Wendys"],
-    }
-    
+    },
   })
 })
 
 //Get individual restaurant route handler
-app.get("/api/v1/restaurants/:restaurantid", (req, res) => {
+app.get("/api/v1/restaurants/:id", (req, res) => {
   console.log(req.params);
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      restaurant: "McDonalds",
+    },
+  })
 });
 
 //Create a restaurant route handler
 app.post("/api/v1/restaurants", (req, res) => {
   console.log(req.body);
+
+  res.status(201).json({
+    status: "success",
+    data: {
+      restaurant: "McDonalds",
+    },
+})
+})
+
+//Update Restaurants
+
+app.put("/api/v1/restaurants/:id", (req, res) => {
+  console.log(req.params.id);
+  console.log(req.body)
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      restaurant: "McDonalds",
+    },
+})
+})
+
+//Delete Restaurants
+
+app.delete("/api/v1/restaurants/:id", (req, res) => {
+  res.status(204).json({
+    status: "success",
+  })
 })
 
 // || default value
