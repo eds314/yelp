ad90e8f6bc614ddee160e1beccff0b6e3b27fd9b
eds314 <eduardoramirez314@gmail.com>
2021-05-28 19:17:02 -0500

    create restaurant through api, return data to front

--- server/server.js
+++ server/server.js
@@ -43,7 +43,7 @@ app.get("/api/v1/restaurants/:id", async (req, res) => {
     const results = await db.query("select * from restaurants where id = $1", [
       req.params.id,
     ]);
-    
+
     res.status(200).json({
       status: "success",
       data: {
@@ -56,15 +56,28 @@ app.get("/api/v1/restaurants/:id", async (req, res) => {
 });
 
 //Create a restaurant route handler
-app.post("/api/v1/restaurants", (req, res) => {
+app.post("/api/v1/restaurants", async (req, res) => {
   console.log(req.body);
 
-  res.status(201).json({
+  try {
+    const results = await db.query(
+      "INSERT INTO restaurants (name, location, price_range) values($1, $2, $3) returning *", [
+      req.body.name,
+      req.body.location,
+      req.body.price_range]
+    )
+    console.log(results)
+    res.status(201).json({
     status: "success",
     data: {
-      restaurant: "McDonalds",
+      restaurant: results.rows[0],
     },
   });
+  } catch (error) {
+    console.log(error)
+  }
+
+  
 });
 
 //Update Restaurants
