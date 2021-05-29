da2c532867caff8b55a844361ffc18d90db4875e
eds314 <eduardoramirez314@gmail.com>
2021-05-29 13:12:56 -0500

    delete restaurant route

--- server/server.js
+++ server/server.js
@@ -101,10 +101,18 @@ app.put("/api/v1/restaurants/:id", async (req, res) => {
 
 //Delete Restaurants
 
-app.delete("/api/v1/restaurants/:id", (req, res) => {
-  res.status(204).json({
+app.delete("/api/v1/restaurants/:id", async (req, res) => {
+  try {
+    const results = await db.query(
+      "DELETE FROM restaurants where id = $1", [req.params.id]
+    )
+    res.status(204).json({
     status: "success",
   });
+  } catch (error) {
+    console.log(error)
+  }
+  
 });
 
 // || default value
