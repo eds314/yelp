a53c7fc576467f40ac708156f1bcf61fa133aacc
eds314 <eduardoramirez314@gmail.com>
2021-05-28 19:00:25 -0500

    get one restaurant route

--- server/db/index.js
+++ server/db/index.js
@@ -1,8 +1,8 @@
 //importing the pg libraryu
-const { Pool } = require('pg')
+const { Pool } = require("pg");
 // creating a new pool that connects to postgres
-const pool = new Pool()
+const pool = new Pool();
 //exporting query object
 module.exports = {
   query: (text, params) => pool.query(text, params),
-}
+};
--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const express = require('express')
+const express = require("express");
 
-const db = require('./db')
+const db = require("./db");
 
-const morgan = require('morgan')
+const morgan = require("morgan");
 
-const app = express()
+const app = express();
 
 //This is middleware. Always define middle ware before route handelrs
 // app.use(morgan("dev"))
@@ -17,30 +17,42 @@ const app = express()
 
 app.use(express.json());
 
-
-//Get all Restuarants rout handler
+//Get all Restuarants route handler
 app.get("/api/v1/restaurants", async (req, res) => {
+  try {
+    const results = await db.query("select * from restaurants");
 
- const results = await db.query("select * from restaurants")
-  console.log(results)
-  res.status(200).json({
-    status: 'success',
-    data: {
-      restaurant: ["McDonalds", "Wendys"],
-    },
-  })
-})
+    console.log(results);
+    res.status(200).json({
+      status: "success",
+      results: results.rows.length,
+      data: {
+        restaurant: results.rows,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 //Get individual restaurant route handler
-app.get("/api/v1/restaurants/:id", (req, res) => {
-  console.log(req.params);
+app.get("/api/v1/restaurants/:id", async (req, res) => {
+  console.log(req.params.id);
 
-  res.status(200).json({
-    status: "success",
-    data: {
-      restaurant: "McDonalds",
-    },
-  })
+  try {
+    const results = await db.query("select * from restaurants where id = $1", [
+      req.params.id,
+    ]);
+    
+    res.status(200).json({
+      status: "success",
+      data: {
+        restaurant: results.rows[0],
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 //Create a restaurant route handler
@@ -52,33 +64,33 @@ app.post("/api/v1/restaurants", (req, res) => {
     data: {
       restaurant: "McDonalds",
     },
-})
-})
+  });
+});
 
 //Update Restaurants
 
 app.put("/api/v1/restaurants/:id", (req, res) => {
   console.log(req.params.id);
-  console.log(req.body)
+  console.log(req.body);
 
   res.status(200).json({
     status: "success",
     data: {
       restaurant: "McDonalds",
     },
-})
-})
+  });
+});
 
 //Delete Restaurants
 
 app.delete("/api/v1/restaurants/:id", (req, res) => {
   res.status(204).json({
     status: "success",
-  })
-})
+  });
+});
 
 // || default value
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
-  console.log(`server is up and listening on port ${port}`)
-})
+  console.log(`server is up and listening on port ${port}`);
+});
