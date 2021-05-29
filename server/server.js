5f6bdbe97a7c0f5ab62b6d30d6c21ff0cac6bcd8
eds314 <eduardoramirez314@gmail.com>
2021-05-29 13:04:12 -0500

    update restaurant route

--- server/server.js
+++ server/server.js
@@ -61,37 +61,42 @@ app.post("/api/v1/restaurants", async (req, res) => {
 
   try {
     const results = await db.query(
-      "INSERT INTO restaurants (name, location, price_range) values($1, $2, $3) returning *", [
-      req.body.name,
-      req.body.location,
-      req.body.price_range]
-    )
-    console.log(results)
+      "INSERT INTO restaurants (name, location, price_range) values($1, $2, $3) returning *",
+      [req.body.name, req.body.location, req.body.price_range]
+    );
+    console.log(results);
     res.status(201).json({
+      status: "success",
+      data: {
+        restaurant: results.rows[0],
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+//Update Restaurants
+
+app.put("/api/v1/restaurants/:id", async (req, res) => {
+  try {
+    const results = await db.query(
+      "UPDATE restaurants SET name = $1, location = $2, price_range = $3 where id = $4 returning *",
+      [req.body.name, req.body.location, req.body.price_range, req.params.id]
+    ); 
+    res.status(200).json({
     status: "success",
     data: {
       restaurant: results.rows[0],
     },
   });
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-
-  
-});
-
-//Update Restaurants
-
-app.put("/api/v1/restaurants/:id", (req, res) => {
   console.log(req.params.id);
   console.log(req.body);
 
-  res.status(200).json({
-    status: "success",
-    data: {
-      restaurant: "McDonalds",
-    },
-  });
+ 
 });
 
 //Delete Restaurants
