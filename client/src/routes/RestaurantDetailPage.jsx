202f06a1bea781510f4da8d08ec2afce8b37dc3b
eds314 <eduardoramirez314@gmail.com>
2021-06-04 13:43:04 -0500

    star rating component

--- client/src/routes/RestaurantDetailPage.jsx
+++ client/src/routes/RestaurantDetailPage.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
+import { useParams } from 'react-router'
+import RestaurantFinder from '../apis/RestaurantFinder';
+import StarRating from '../components/StarRating';
+import { RestaurantsContext } from '../context/RestaurantsContext';
 
 const RestaurantDetailPage = () => {
-  return (
-    <div>
-      Detail page
-    </div>
+  const{id} = useParams();
+  const {selectedRestaurant, setSelectedRestaurant} = useContext(
+    RestaurantsContext
   )
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await RestaurantFinder.get(`/${id}`)
+
+        setSelectedRestaurant(response.data.data.restaurant)
+      } catch (error) {
+        console.log(error)
+      }
+    
+    }
+    fetchData()
+  }, [])
+
+  return <div>{selectedRestaurant && <StarRating rating={1} /> }</div>
 }
 
 export default RestaurantDetailPage
