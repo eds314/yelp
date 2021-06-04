ff56600eaa91111542d62716a353c2da310c7ad6
eds314 <eduardoramirez314@gmail.com>
2021-06-04 17:24:11 -0500

    add review functionality

--- client/src/components/AddReview.jsx
+++ client/src/components/AddReview.jsx
@@ -1,10 +1,30 @@
 import React, { useState } from "react";
+import RestaurantFinder from "../apis/RestaurantFinder";
+import { useHistory, useLocation, useParams } from "react-router-dom";
 
 const AddReview = () => {
+  const { id } = useParams();
+  const location = useLocation();
+  console.log(location)
+  const history = useHistory();
+  console.log(id);
+
   const [name, setName] = useState * "";
   const [reviewText, setReviewText] = useState * "";
   const [rating, setRating] = useState("Rating");
 
+  const handleSubmitReview = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await RestaurantFinder.post(`/${id}/addReview`, {
+        name,
+        review: reviewText,
+        rating,
+      });
+      history.push("/")
+      history.push(location.pathname)
+    } catch (error) {}
+  };
   return (
     <div className="mb-2">
       <form action="">
@@ -20,11 +40,12 @@ const AddReview = () => {
           </div>
           <div className="from-group col-4">
             <label htmlFor="rating">Rating</label>
-            <select 
-            value={rating}
-            onChange= {(e) => setRating(e.target.value)}
-            id="rating" 
-            className="custom-select">
+            <select
+              value={rating}
+              onChange={(e) => setRating(e.target.value)}
+              id="rating"
+              className="custom-select"
+            >
               <option disabled>Rating</option>
               <option value="1">1</option>
               <option value="2">2</option>
@@ -37,12 +58,19 @@ const AddReview = () => {
         <div className="form-group">
           <label htmlFor="Reviews">Reviews</label>
           <textarea
-          value={reviewText}
-          onChange={(e) => setReviewText(e.target.value)} 
-          id="Review" 
-          className="form-control"></textarea>
+            value={reviewText}
+            onChange={(e) => setReviewText(e.target.value)}
+            id="Review"
+            className="form-control"
+          ></textarea>
         </div>
-        <button className="btn btn-primary">Submit</button>
+        <button
+          typeof="submit"
+          onClick={handleSubmitReview}
+          className="btn btn-primary"
+        >
+          Submit
+        </button>
       </form>
     </div>
   );
